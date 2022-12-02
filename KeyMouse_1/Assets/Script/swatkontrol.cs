f2b6fadd086f15361f4332070f0859df5b49b4ab
ReisgoldmanX <46068107+reisgoldmanX@users.noreply.github.com>
2022-12-01 21:48:04 -0500

    UI added
    Uİ added with a pause key scripted.
    which is "ESC"

--- KeyMouse_1/Assets/Script/swatkontrol.cs
+++ KeyMouse_1/Assets/Script/swatkontrol.cs
@@ -8,23 +8,29 @@ public class swatkontrol : MonoBehaviour
     void Start()
     {
         swat = GetComponent<Animator>();
+        Time.timeScale = 0;
     }
 
     
     void Update()
     {
+        Swat_control();
+       
+    }
+    void Swat_control()
+    {
+        
         if (Input.GetKey(KeyCode.W))
         {
-            swat.SetFloat("hiz",0.4f);
-            if (Input.GetKey(KeyCode.LeftShift)&&Input.GetKey(KeyCode.W))
+            swat.SetFloat("hiz", 0.4f);
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
             {
                 swat.SetFloat("hiz", 1f);
             }
         }
-        else 
+        else
         {
             swat.SetFloat("hiz", 0f);
         }
-       
     }
 }
