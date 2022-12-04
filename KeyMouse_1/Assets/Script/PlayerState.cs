a89b6cf2e888edeb5838872b43c87ef464fcf87d
ReisgoldmanX <46068107+reisgoldmanX@users.noreply.github.com>
2022-12-04 17:01:34 -0500

    eh

--- KeyMouse_1/Assets/Script/PlayerState.cs
+++ KeyMouse_1/Assets/Script/PlayerState.cs
@@ -22,18 +22,13 @@ public class PlayerState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.R) && Ammo != 30)
-        {
-            // Reload by user
-            Ammo = 30;
-            Ammo_text.text = Ammo.ToString();
-        }
+        
         
         if (Input.GetMouseButtonDown(0))
         {
             if (Ammo <= 0)
             {
-                // Reload out of ammo
+                // Reload
                 Ammo = 30;
             }
             else
