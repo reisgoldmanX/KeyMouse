dc680f345ec25e96e3d5c36dba5b697e7e07e4f6
ReisgoldmanX <46068107+reisgoldmanX@users.noreply.github.com>
2022-12-04 16:27:36 -0500

    Fix
    Fix

--- KeyMouse_1/Assets/Script/swatkontrol.cs
+++ KeyMouse_1/Assets/Script/swatkontrol.cs
@@ -27,8 +27,6 @@ public class swatkontrol : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
             {
                 swat.SetFloat("hiz", 1f); // Running Speed
-
-                maxspeed = 0.3f;
             }
         }
 
@@ -56,16 +54,14 @@ public class swatkontrol : MonoBehaviour
             // Stand still (no movement)
             swat.SetFloat("hiz", 0f);
 
-            swat.SetFloat("hiz", 0f);
-            maxspeed = 0f;
             //axisZ = maxspeed * Input.GetAxis("Vertical");
 
         }
-        Vector3 vector = new Vector3(0,0,axisZ);
+        // Vector3 vector = new Vector3(0,0,axisZ);
 
         // Animasyonlar arasý geçiþi düzenler
-        swat.SetFloat("hiz", Vector3.ClampMagnitude(vector,1f).magnitude,1, Time.deltaTime);
-        swat.SetFloat("hiz", maxspeed);
+        //swat.SetFloat("hiz", Vector3.ClampMagnitude(vector,1f).magnitude,1, Time.deltaTime);
+        //swat.SetFloat("hiz", maxspeed);
 
     }
 }
